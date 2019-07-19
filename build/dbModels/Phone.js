c87866840ae0d0fd45fe7819ca28ddf6a86f7383
Joel Rivera <tilyxxx@gmail.com>
2019-07-19 04:37:54 -0400

    deleted compiled js files

--- build/dbModels/Phone.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Phone = /** @class */ (function () {
-    function Phone() {
-    }
-    return Phone;
-}());
-exports.Phone = Phone;
